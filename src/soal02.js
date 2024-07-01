8e56d24e9d425c3cc26301ac1b1072d75dd84df8
wahyuridho <saputroridho569@gmail.com>
2024-07-01 16:06:42 +0700

    menambahkan file productBin.json untuk menyimpan data reponse dari API, dan menambahkan code untuk penyelesaian soal 02

---
+++ src/soal02.js
@@ -0,0 +1,39 @@
+// Import
+const fs = require('fs');
+
+// Membaca file json productBin
+fs.readFile('./src/productBin.json', 'utf8', (err, jsonString) => {
+    
+    // Cetak error jika gagal membaca file json
+    if (err) {
+        console.error('Error reading file:', err);
+        return;
+    }
+
+    // lakukan try
+    try {
+        var totalQuantity = 0;
+
+        // Parsing data json
+        const productBin = JSON.parse(jsonString);
+
+        // Lakukan perulangan untuk mendapatkan value tiap element dari array data
+        productBin.data.forEach(element => {
+            var productCode = element.productCode;
+            var quantity = element.quantity;
+
+            // ambil data jika codenya FBR00040101
+            if(productCode = 'FBR00040101'){
+                // menjumlahkan Quantity dari tiap element
+                totalQuantity = totalQuantity + quantity;
+            }
+        });
+
+        // Cetak hasil penjumlahan quantity yang memiliki productcode FBR00040101
+        console.log(`Total dari barang dengan ProductCode FBR00040101 adalah : ${totalQuantity}`);
+
+    } catch (error) {
+        // Print Error jika gagal parsing data json
+        console.error('Error parsing JSON:', error);
+    }
+});
